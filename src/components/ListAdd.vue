dd756743f93983bff3d7444909f0b063499cb0b5
yuyaamano <nba.chako.nba@icloud.com>
2020-10-11 20:05:38 +0900

    4-22--2①フォームがフォーカスされたらフォームを活性に、フォーカスアウトされたら

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,30 +1,44 @@
 <template>
-  <!-- ★ここから追記 -->
-  <form class="addlist" @submit.prevent="addList">
+  <form :class="classList" @submit.prevent="addList">
     <input
       v-model="title"
       type="text"
       class="text-input"
       placeholder="Add new list"
+      @focusin="startEditing"
+      @focusout="finishEditing"
     />
     <button type="submit" class="add-button">Add</button>
   </form>
-  <!-- ★ここまで追記 -->
 </template>
 <script>
 export default {
   data: function () {
     return {
       title: "",
+      isEditing: false,
     };
   },
-  // ★ここから追記
+  computed: {
+    classList() {
+      const classList = ["addlist"];
+      if (this.isEditing) {
+        classList.push("active");
+      }
+      return classList;
+    },
+  },
   methods: {
     addList: function () {
       this.$store.dispatch("addlist", { title: this.title });
       this.title = "";
     },
+    startEditing() {
+      this.isEditing = true;
+    },
+    finishEditing() {
+      this.isEditing = false;
+    },
   },
-  // ★ここまで追記
 };
 </script>
