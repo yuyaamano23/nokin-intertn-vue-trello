a2c69dae00e2447eff767bfe1717972a86102039
yuyaamano <nba.chako.nba@icloud.com>
2020-10-11 18:59:10 +0900

    3-1②Board.vueに<template>内にヘッダータグとメインタグを追記

---
+++ src/components/Board.vue
@@ -0,0 +1,10 @@
+<template>
+  　<!-- ここから追加 -->
+  <div>
+    <header>my Trello</header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+    </main>
+  </div>
+  <!-- ここまで追加 -->
+</template>
