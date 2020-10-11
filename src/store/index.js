343d46815317b48459c0a8b32c81b084d60b1908
yuyaamano <nba.chako.nba@icloud.com>
2020-10-11 19:54:45 +0900

    4-1③ストアのデータの状態をlocalStorageに保存しよう

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,28 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+const savedLists = localStorage.getItem("trello-lists"); // ★①追加
+const store = new Vuex.Store({
+  // ★②編集
   state: {
-    lists: [],
+    // ★ここから③編集
+    lists: savedLists
+      ? JSON.parse(savedLists)
+      : [
+          {
+            title: "Backlog",
+            cards: [{ body: "English" }, { body: "Mathematics" }],
+          },
+          {
+            title: "Todo",
+            cards: [{ body: "Science" }],
+          },
+          {
+            title: "Doing",
+            cards: [],
+          },
+        ],
+    // ★ここまで③編集
   },
   mutations: {
     addlist(state, payload) {
@@ -17,6 +36,13 @@ export default new Vuex.Store({
       context.commit("addlist", payload);
     },
   },
-  // ★modulesをgettersに修正
   getters: {},
 });
+
+// ★ここから追記
+store.subscribe((mutation, state) => {
+  localStorage.setItem("trello-lists", JSON.stringify(state.lists));
+});
+
+export default store;
+// ★ここまで追記
