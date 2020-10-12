6a037f5ff946bb49e0adf6ff27678b29608acaf8
yuyaamano <nba.chako.nba@icloud.com>
2020-10-12 15:09:12 +0900

    5-1①「フォームボタンが押下されたら、カードを新しく作成する」仕組みを実装しよう（CardAdd.vue）

---
+++ src/components/CardAdd.vue
@@ -0,0 +1,36 @@
+<template>
+  <form class="addcard" @submit.prevent="addCardToList">
+    <input
+      v-model="body"
+      type="text"
+      class="text-input"
+      placeholder="Add new card"
+    />
+    <button type="submit" class="add-button">Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    listIndex: {
+      type: Number,
+      required: true,
+    },
+  },
+  data: function () {
+    return {
+      body: "",
+    };
+  },
+  methods: {
+    addCardToList: function () {
+      this.$store.dispatch("addCardToList", {
+        body: this.body,
+        listIndex: this.listIndex,
+      });
+      this.body = "";
+    },
+  },
+};
+</script>
--- src/components/List.vue
+++ src/components/List.vue
@@ -4,11 +4,17 @@
       <p class="list-title">{{ title }}</p>
       <div class="deletelist" @click="removeList">×</div>
     </div>
+    <card-add :listIndex="listIndex" />
   </div>
 </template>
 
 <script>
+import CardAdd from "./CardAdd";
+
 export default {
+  components: {
+    CardAdd,
+  },
   props: {
     title: {
       type: String,
